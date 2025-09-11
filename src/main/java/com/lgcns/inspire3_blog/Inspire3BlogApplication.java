1ac7fe39aeec292255eb4c695c64487fc15c8773
aaahyunseo <hs030608@naver.com>
2025-09-11 17:13:31 +0900

    init : 초기 세팅

---
+++ src/main/java/com/lgcns/inspire3_blog/Inspire3BlogApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.inspire3_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inspire3BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inspire3BlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgcns/inspire3_blog/Inspire3BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.inspire3_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inspire3BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
