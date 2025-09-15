769138f1df6166193ea031d4996afdc874cc3409
Hong9915 <hhj4498@naver.com>
2025-09-15 13:43:53 +0900

    feat: OpenAI 연동 기능 강의내용 요약, 오늘의 운세  추가

--- src/main/java/com/lgcns/inspire3_blog/Inspire3BlogApplication.java
+++ src/main/java/com/lgcns/inspire3_blog/Inspire3BlogApplication.java
@@ -3,11 +3,18 @@ package com.lgcns.inspire3_blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class Inspire3BlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Inspire3BlogApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        Dotenv env = Dotenv.configure().ignoreIfMissing().load();
+		env.entries().forEach(entry -> 
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+
+        SpringApplication.run(Inspire3BlogApplication.class, args);
+    }
 }
